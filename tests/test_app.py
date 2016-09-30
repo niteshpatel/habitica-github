6bd450b7f5c6653a09f84b0a0592d54bf9e9ae53
Nitesh Patel <github@nitesh.name>
2016-09-30 05:47:51 +0100

    Add test for score event task

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,8 @@
+import json
 from unittest import TestCase
 
 import requests_mock
+from mock import mock
 
 import app
 
@@ -13,26 +15,52 @@ class AppTestCase(TestCase):
         app.HABITICA_API_USER = self.apiUser
         app.HABITICA_API_KEY = self.apiKey
 
+        self.app = app.app.test_client()
+
     @requests_mock.mock()
-    def test_score_task(self, m):
+    def test_score_task_event(self, m):
         # arrange
-        expected_headers = {
-            'x-api-user': self.apiUser,
-            'x-api-key': self.apiKey
-        }
+        data = json.dumps({
+            'commits': [
+                {
+                    'author': {
+                        'email': 'nitesh'
+                    }
+                }
+            ]})
 
-        expected_url = 'https://habitica.com/api/v3/tasks/task1/score/up'
-
-        m.post(requests_mock.ANY, text="{}")
+        expected_response = {'test': 'value'}
+        app.score_task = mock.Mock(return_value=expected_response)
 
         # act
-        app.score_task('task1', 'up')
+        rv = self.app.post('/tasks/task1/score/up', data=data, content_type='application/json')
 
         # assert
-        history = m.request_history
-        self.assertEqual(len(history), 1)
+        # noinspection PyUnresolvedReferences
+        app.score_task.assert_called_once_with('task1', 'up')
+        json_data = json.loads(rv.data)
+        self.assertEqual(json_data, [expected_response])
 
-        request = history[0]
-        self.assertEqual(request.url, expected_url)
-        self.assertEqual(request.method, 'POST')
-        self.assertDictContainsSubset(expected_headers, request.headers)
+        @requests_mock.mock()
+        def test_score_task(self, m):
+            # arrange
+            expected_headers = {
+                'x-api-user': self.apiUser,
+                'x-api-key': self.apiKey
+            }
+
+            expected_url = 'https://habitica.com/api/v3/tasks/task1/score/up'
+
+            m.post(requests_mock.ANY, text="{}")
+
+            # act
+            app.score_task('task1', 'up')
+
+            # assert
+            history = m.request_history
+            self.assertEqual(len(history), 1)
+
+            request = history[0]
+            self.assertEqual(request.url, expected_url)
+            self.assertEqual(request.method, 'POST')
+            self.assertDictContainsSubset(expected_headers, request.headers)
