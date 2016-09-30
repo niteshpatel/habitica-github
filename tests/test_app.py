3222271f0c937c6f61f0b2f35a1bf1c005100f9d
Nitesh Patel <github@nitesh.name>
2016-09-30 05:13:57 +0100

    Unit test for score_task

---
+++ tests/test_app.py
@@ -0,0 +1,38 @@
+from unittest import TestCase
+
+import requests_mock
+
+import app
+
+
+class AppTestCase(TestCase):
+    def setUp(self):
+        self.apiUser = 'joe'
+        self.apiKey = 'secret'
+
+        app.HABITICA_API_USER = self.apiUser
+        app.HABITICA_API_KEY = self.apiKey
+
+    @requests_mock.mock()
+    def test_send_message_to_habitica_from_user(self, m):
+        # arrange
+        expected_headers = {
+            'x-api-user': self.apiUser,
+            'x-api-key': self.apiKey
+        }
+
+        expected_url = 'https://habitica.com/api/v3/tasks/task1/score/up'
+
+        m.post(requests_mock.ANY, text="{}")
+
+        # act
+        app.score_task('task1', 'up')
+
+        # assert
+        history = m.request_history
+        self.assertEqual(len(history), 1)
+
+        request = history[0]
+        self.assertEqual(request.url, expected_url)
+        self.assertEqual(request.method, 'POST')
+        self.assertDictContainsSubset(expected_headers, request.headers)
